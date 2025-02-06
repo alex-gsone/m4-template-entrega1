51ef9d283cb67fea5b9fbab40d030c524d29b6c9
Alex Gomes <alex.gsone@gmail.com>
2025-02-06 08:07:05 -0300

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Importando as interfaces:
+import { Product, HandleProducts} from './interfaces';
+
+
+class ProductList implements HandleProducts{
+
+    private ProductList: Product[] = [];
+    public id: number = 1;
+
+    createProduct(data: { name: string; price: number}): Product{
+        
+        return 
+    }
+        
+    }
+
+}
+
